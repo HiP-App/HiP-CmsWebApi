0c3c523e7b27ac3d4a0e0efe196da6a342ccb6b2
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-06-22 14:58:39 +0200

    HiPCMS - 368 Adding Integration Tests

---
+++ test/Api.IntegrationTests/TestStartup.cs
@@ -0,0 +1,23 @@
+﻿using Api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.IntegrationTests
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IHostingEnvironment env)
+            : base(env)
+        {
+        }
+
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+            // Register here the dependency injection services
+            services.AddDbContext<ApplicationDbContext>(opts => opts.UseInMemoryDatabase());
+        }
+    }
+}
