1f544dbbb307a687ce0c4e881e2f489d2adaefdd
uxman-sherwani <uxman.sherwani@gmail.com>
2016-07-06 07:59:04 +0200

    Updating dev configs to make it consistent with docker env variables

--- src/Api/Utility/AppConfig.cs
+++ src/Api/Utility/AppConfig.cs
@@ -1,23 +1,67 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace Api.Utility
 {
     public class AppConfig
     {
-        public string ClientId { get; set; }
+        public DatabaseConfig DatabaseConfig { get; set; }
 
-        public string Domain { get; set; }
-
+        public AuthConfig AuthConfig { get; set; }
+        
         public bool RequireHttpsMetadata { get; set; }
 
         public string AdminEmail { get; set; }
 
         public AppConfig(IConfigurationRoot configuration)
         {
-            ClientId = configuration.GetValue<string>("Configurations:Auth:ClientId");
-            Domain = configuration.GetValue<string>("Configurations:Auth:Domain");
-            RequireHttpsMetadata = configuration.GetValue<bool>("Configurations:RequireHttpsMetadata");
-            AdminEmail = configuration.GetValue<string>("Configurations:AdminEmail");
+            DatabaseConfig = new DatabaseConfig
+            {
+                Host = configuration.GetValue<string>("DB_HOST"),
+                Username = configuration.GetValue<string>("DB_USERNAME"),
+                Password = configuration.GetValue<string>("DB_PASSWORD"),
+                Name = configuration.GetValue<string>("DB_NAME")
+            };
+
+            AuthConfig = new AuthConfig 
+            {
+                ClientId = configuration.GetValue<string>("CLIENT_ID"),
+                Domain = configuration.GetValue<string>("DOMAIN")
+            };
+
+            RequireHttpsMetadata = !configuration.GetValue<bool>("ALLOW_HTTP");
+            AdminEmail = configuration.GetValue<string>("ADMIN_EMAIL");
         }
+    }
+
+    public class DatabaseConfig
+    {
+        public string Name { get; set; }
+
+        public string Host { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConnectionString { 
+            get {
+                StringBuilder connectionString = new StringBuilder();
+                
+                connectionString.Append($"Host={Host};");
+                connectionString.Append($"Username={Username};");
+                connectionString.Append($"Password={Password};");
+                connectionString.Append($"Database={Name}");
+                
+                return connectionString.ToString();
+            }
+        }
+    }
+
+    public class AuthConfig
+    {
+        public string ClientId { get; set; }
+
+        public string Domain { get; set; }
     }
 }
