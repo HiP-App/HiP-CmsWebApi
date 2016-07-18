b5316cdf642406e55ea34485eed8f5c5c286dbf5
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-07-18 19:47:48 +0200

    HiPCMS - 108 Modifying the Many to Many Relationship

---
+++ src/BOL/Models/Topic/TopicListDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using BOL.Models;
+
+namespace BOL.Models
+{
+    public class TopicListDTO
+    {
+        
+    }
+}
