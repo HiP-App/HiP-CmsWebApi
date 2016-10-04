84f269bd13ddf3ea8a66d6c940ac6c4c26bab13e
sstamm <sstamm@mail.upb.de>
2016-10-04 20:04:43 +0200

    HIPCMS-234 test

--- src/Api/Data/DbTask.cs
+++ src/Api/Data/DbTask.cs
@@ -16,8 +16,9 @@ namespace Api.Data
             var userManager = new UserManager(dbContext);
 
             // Run Migrations to apply any pending migrations. (if any)
-            //dbContext.Database.Migrate();
+
             dbContext.Database.EnsureCreated();
+            //dbContext.Database.Migrate();
 
             // Seed Db with Admin Account if its not already created.
             var admin = userManager.GetUserByEmailAsync(appConfig.AdminEmail).Result;
