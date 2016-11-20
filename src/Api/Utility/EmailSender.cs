16ca98433a7c3d555b63f3e93d094e101260501d
Janis Krasemann <janis.krasemann@enpit.de>
2016-11-20 16:32:47 +0100

    HIPCMS-249 Removed unnecessary using directives

--- src/Api/Utility/EmailSender.cs
+++ src/Api/Utility/EmailSender.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using MailKit.Net.Smtp;
-using MailKit;
 using MimeKit;
-using Microsoft.AspNetCore.Builder;
 using System.IO;
 
 namespace Api.Utility
