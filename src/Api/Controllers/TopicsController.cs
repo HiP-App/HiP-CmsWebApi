c081eb1486b0c39bf892059e79306d74cdf67426
nagaraj1993 <nagaraj@mail.uni-paderborn.de>
2016-11-23 23:12:13 +0100

    HIPCMS-473

--- src/Api/Controllers/TopicsController.cs
+++ src/Api/Controllers/TopicsController.cs
@@ -33,6 +33,16 @@ namespace Api.Controllers
         #region GET topics
 
         // GET api/topics
+
+        /// <summary>
+        /// All topics matching query, status and deadline
+        /// </summary>   
+        /// <param name="query">Topics containing query in Title and Description</param>
+        /// <param name="status">Topics in status </param>
+        /// <param name="deadline">Topics with deadline </param>
+        /// <param name="onlyParents">Indicates to get only parent topics</param>
+        /// <param name="page">Represents the page</param>
+        /// <response code="200">Returns PagedResults of TopicResults</response>        
         [HttpGet]
         [ProducesResponseType(typeof(PagedResult<TopicResult>), 200)]
         public IActionResult Get(string query, string status, DateTime? deadline, bool onlyParents = false, int page = 1)
@@ -43,7 +53,13 @@ namespace Api.Controllers
             return Ok(new PagedResult<TopicResult>(entities, page, count));
         }
 
-        // GET api/topics
+        // GET api/topics/OfUser/Current
+
+        /// <summary>
+        /// All topics of the current user
+        /// </summary>
+        /// <param name="page">Represents the page</param>
+        /// <response code="200">Returns PagedResults of TopicResults</response>        
         [HttpGet("OfUser/Current")]
         [ProducesResponseType(typeof(PagedResult<TopicResult>), 200)]
         public IActionResult GetTopicsForUser(int page = 1)
@@ -51,7 +67,14 @@ namespace Api.Controllers
             return GetTopicsForUser(User.Identity.GetUserId(), page);
         }
 
-        // GET api/topics
+        // GET api/topics/OfUser/:userId
+
+        /// <summary>
+        /// All topics of the user {userId}
+        /// </summary>
+        /// <param name="userId">Represents the user Id of the user</param>
+        /// <param name="page">Represents the page</param>
+        /// <response code="200">Returns PagedResults of TopicResults</response>        
         [HttpGet("OfUser/{userId}")]
         [ProducesResponseType(typeof(PagedResult<TopicResult>), 200)]
         public IActionResult GetTopicsForUser(int userId, int page = 1)
@@ -63,6 +86,12 @@ namespace Api.Controllers
         }
 
         // GET api/topics/:topicId
+
+        /// <summary>
+        /// Retrieves the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">Represents the user Id of the user</param>        
+        /// <response code="200">Returns the topic {topicId}</response>        
         [HttpGet("{topicId}")]
         public IActionResult Get(int topicId)
         {
@@ -76,7 +105,13 @@ namespace Api.Controllers
 
         #region AssociatedTopics
 
+            #region GET
         // GET api/topics/:topicId/subtopics
+
+        /// <summary>
+        /// Retrieves the subtopics of the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
         [HttpGet("{topicId}/SubTopics")]
         public IActionResult GetSubTopics(int topicId)
         {
@@ -85,13 +120,31 @@ namespace Api.Controllers
 
 
         // GET api/topics/:topicId/parenttopics
+
+        /// <summary>
+        /// Retrieves the parent topics of the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>
         [HttpGet("{topicId}/ParentTopics")]
         public IActionResult GetParentTopics(int topicId)
         {
             return Ok(topicManager.GetParentTopics(topicId));
         }
 
+        #endregion
+
+            #region PUT
+
         // PUT api/topics/:topicId/ParentTopics/:parentId
+
+        /// <summary>
+        /// Associates the topic {topicId} with parent topic {parentId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic to be associated</param>
+        /// <param name="parentId">The Id of the parent topic</param>
+        /// <response code="200">Topics {topicId} and {parentId} associated successfully</response>        
+        /// <response code="400">Request incorrect</response>        
+        /// <response code="403">User not allowed to associate topics</response>        
         [HttpPut("{topicId}/ParentTopics/{parentId}")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 400)]
@@ -108,6 +161,15 @@ namespace Api.Controllers
         }
 
         // PUT api/topics/:topicId/SubTopics/:parentId
+
+        /// <summary>
+        /// Associates the topic {topicId} with child topic {childId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic to be associated</param>
+        /// <param name="childId">The Id of the child topic</param>
+        /// <response code="200">Topics {topicId} and {childId} associated successfully</response>        
+        /// <response code="400">Request incorrect</response>        
+        /// <response code="403">User not allowed to associate topics</response>        
         [HttpPut("{topicId}/SubTopics/{childId}")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 400)]
@@ -123,7 +185,19 @@ namespace Api.Controllers
             return BadRequest(result);
         }
 
-        // DELETE api/topics/:id
+        #endregion
+
+            #region DELETE
+
+        // DELETE api/topics/:id/ParentTopics/:parentId
+
+        /// <summary>
+        /// Deletes the associated topic {topicId} and parent topic {parentId}
+        /// </summary>
+        /// <param name="topicId">The Id of the child topic</param>
+        /// <param name="parentId">The Id of the parent topic</param>
+        /// <response code="200">Deleted Associated parent {parentId} and child {topicId}</response>        
+        /// <response code="404">Not found</response>                      
         [HttpDelete("{topicId}/ParentTopics/{parentId}")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 404)]
@@ -137,7 +211,16 @@ namespace Api.Controllers
             return NotFound();
         }
 
-        // DELETE api/topics/:id
+        // DELETE api/topics/:id/SubTopics/:childId
+
+        /// <summary>
+        /// Deletes the associated topic {topicId} and child topic {childId}
+        /// </summary>
+        /// <param name="topicId">The Id of the parent topic</param>
+        /// <param name="childId">The Id of the child topic</param>
+        /// <response code="200">Deleted Associated parent {topicId} and child {childId}</response>        
+        /// <response code="400">Request incorrect</response>        
+        /// <response code="404">Not found</response>                      
         [HttpDelete("{topicId}/SubTopics/{childId}")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 400)]
@@ -154,9 +237,17 @@ namespace Api.Controllers
 
         #endregion
 
+        #endregion
+
         #region Get Users
 
         // GET api/topics/:topicId/students
+
+        /// <summary>
+        /// All students associated with the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
+        /// <response code="200">A list of students assocaited with the topic {topicId}</response>        
         [HttpGet("{topicId}/Students")]
         [ProducesResponseType(typeof(IEnumerable<UserResult>), 200)]
         public IActionResult GetTopicStudents(int topicId)
@@ -165,6 +256,12 @@ namespace Api.Controllers
         }
 
         // GET api/topics/:topicId/supervisors
+
+        /// <summary>
+        /// All supervisors associated with the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
+        /// <response code="200">A list of supervisors assocaited with the topic {topicId}</response>        
         [HttpGet("{topicId}/Supervisors")]
         [ProducesResponseType(typeof(IEnumerable<UserResult>), 200)]
         public IActionResult GetTopicSupervisors(int topicId)
@@ -173,6 +270,12 @@ namespace Api.Controllers
         }
 
         // GET api/topics/:topicId/reviewers
+
+        /// <summary>
+        /// All reviewers associated with the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
+        /// <response code="200">A list of reviewers assocaited with the topic {topicId}</response>        
         [HttpGet("{topicId}/Reviewers")]
         [ProducesResponseType(typeof(IEnumerable<UserResult>), 200)]
         public IActionResult GetTopicReviewers(int topicId)
@@ -190,6 +293,15 @@ namespace Api.Controllers
         #region PUT users
 
         // PUT api/topics/:topicId/students
+
+        /// <summary>
+        /// Edit assigned students for the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
+        /// <param name="users">An array of users</param>        
+        /// <response code="200">Edited assigned students for the topic {topicId}</response>        
+        /// <response code="400">Request incorrect</response>        
+        /// <response code="403">User not allowed to edit topic</response>        
         [HttpPut("{topicId}/Students")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 400)]
@@ -200,6 +312,15 @@ namespace Api.Controllers
         }
 
         // PUT api/topics/:topicId/Supervisors
+
+        /// <summary>
+        /// Edit supervisors for the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
+        /// <param name="users">An array of users</param>        
+        /// <response code="200">Edited supervisors for the topic {topicId}</response>        
+        /// <response code="400">Request incorrect</response>        
+        /// <response code="403">User not allowed to edit topic</response>        
         [HttpPut("{topicId}/Supervisors")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 400)]
@@ -210,6 +331,15 @@ namespace Api.Controllers
         }
 
         // PUT api/topics/:topicId/Reviewers
+
+        /// <summary>
+        /// Edit reviewers for the topic {topicId}
+        /// </summary>
+        /// <param name="topicId">The Id of the topic</param>        
+        /// <param name="users">An array of users</param>        
+        /// <response code="200">Edited reviewers for the topic {topicId}</response>        
+        /// <response code="400">Request incorrect</response>        
+        /// <response code="403">User not allowed to edit topic</response>        
         [HttpPut("{topicId}/Reviewers")]
         [ProducesResponseType(typeof(void), 200)]
         [ProducesResponseType(typeof(void), 400)]
