40dcf25fa0172f5d14aad9c90d68c5f2fed69a3a
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2016-12-19 14:33:51 +0100

    ISS-HIPCMS-249 Did `dotnet ef migrations add subscribe-notifications`

--- src/Api/Migrations/20161219133316_subscribe-notifications.Designer.cs
+++ src/Api/Migrations/20161219133316_subscribe-notifications.Designer.cs
@@ -8,9 +8,10 @@ using Api.Data;
 namespace Api.Migrations
 {
     [DbContext(typeof(CmsDbContext))]
-    partial class CmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161219133316_subscribe-notifications")]
+    partial class subscribenotifications
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
