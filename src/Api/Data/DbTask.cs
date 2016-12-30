f76ab2d677894ab42de69cf8b639f349092748af
nagaraj1993 <nagaraj@mail.uni-paderborn.de>
2016-12-30 11:21:09 +0100

    HIPCMS-30-Own-TestStartup-Class

--- src/Api/Data/DbTask.cs
+++ src/Api/Data/DbTask.cs
@@ -12,22 +12,14 @@ namespace Api.Data
 {
     public static class DbTask
     {
-        public static void RunMigrationsAndSeedDb(this IApplicationBuilder app, IHostingEnvironment env)
+        public static void RunMigrationsAndSeedDb(this IApplicationBuilder app)
         {
             var dbContext = app.ApplicationServices.GetRequiredService<CmsDbContext>();
             var appConfig = app.ApplicationServices.GetRequiredService<AppConfig>();
             var userManager = new UserManager(dbContext);
-            var setEnvironmentAsTest = "Test";
 
-            //Basically we need the below check because the framework is using an In-memory database and hence it does not need the below call. 
-            //Therefore we need a check checking if the environment is Test or not.
-            //The default environment on which the tests are executed is 'Test'.
-            //So only for this Test environment, the migrations won't apply and it will be applied for other environments. Eg: Production or Development or Staging
-            if (!(env.EnvironmentName == setEnvironmentAsTest))
-            {
-                // Run Migrations to apply any pending migrations. (if any)
-                dbContext.Database.Migrate();
-            }
+            // Run Migrations to apply any pending migrations. (if any)
+            dbContext.Database.Migrate();
 
             // Seed Db with Admin Account if its not already created.
             try
--- test/Api.Tests/TestStartup.cs
+++ test/Api.Tests/TestStartup.cs
@@ -1,20 +1,69 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Api.Data;
+using Api.Utility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Api.Tests
 {
-    public class TestStartup : Startup
+    public class TestStartup
     {
-        public TestStartup(IHostingEnvironment env) : base(env) { }
-
-        public void ConfigureTestServices(IServiceCollection services)
+        public TestStartup(IHostingEnvironment env)
         {
-            base.ConfigureServices(services);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)                
+                .AddEnvironmentVariables();
 
-            // Register here the dependency injection services
-            // services.AddDbContext<ApplicationDbContext>(opts => opts.UseInMemoryDatabase());
-            // Not required because the framework internally uses InMemoryDatabase
-            //https://github.com/ivaylokenov/MyTested.AspNetCore.Mvc/blob/development/samples/MusicStore/MusicStore.Test/TestStartup.cs
+            Configuration = builder.Build();
         }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Read configurations from json
+            var appConfig = new AppConfig(Configuration);
+
+            // Register AppConfig in Services 
+            services.AddSingleton(appConfig);
+
+            // Add database service for Postgres
+            services.AddDbContext<CmsDbContext>(opts => opts.UseInMemoryDatabase());
+
+            // Add framework services
+            services.AddMvc();
+
+            services.AddTransient<EmailSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, AppConfig appConfig)
+        {
+            app.UseMvc();
+        }
+
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<TestStartup>()
+                .Build();
+
+            host.Run();
+        }        
     }
 }
