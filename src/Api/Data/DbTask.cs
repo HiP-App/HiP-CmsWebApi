165691b23a9fc07d5f78bf79ead0591002d526b2
sstamm <sstamm@mail.upb.de>
2017-01-12 11:39:21 +0100

    Update DbTask.cs

--- src/Api/Data/DbTask.cs
+++ src/Api/Data/DbTask.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Api.Models.Entity;
 using System;
-using Microsoft.AspNetCore.Hosting;
 
 namespace Api.Data
 {
