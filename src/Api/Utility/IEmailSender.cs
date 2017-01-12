79bd563dcb4d9627908ecf7ad09644e0ed09c979
sstamm <sstamm@mail.upb.de>
2017-01-12 11:35:25 +0100

    HIPANGULAR-30 updated Appsettings etc. (#45)
    * ...
    
    * HIPANGULAR-30 edit configs

---
+++ src/Api/Utility/IEmailSender.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Utility
+{
+    public interface IEmailSender
+    {
+        Task InviteAsync(string email);
+
+        Task NotifyAsync(string email, Notification notification);
+    }
+}
