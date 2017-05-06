ca6dea1b2f35dfe15294c524b93e9c3d5608bae5
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2017-05-06 15:15:54 +0200

    Added the PlatformAbstractions import again

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
+using Microsoft.Extensions.PlatformAbstractions;
 using PaderbornUniversity.SILab.Hip.CmsApi.Services;
 using Swashbuckle.AspNetCore.Swagger;
 
