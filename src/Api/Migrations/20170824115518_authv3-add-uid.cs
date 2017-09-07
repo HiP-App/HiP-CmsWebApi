fb8689317f211d0b40ca0b8d675a5d92688dffcf
Janis Krasemann <janis.krasemann@enpit.de>
2017-09-07 14:56:32 +0200

    Added migrations

---
+++ src/Api/Migrations/20170824115518_authv3-add-uid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaderbornUniversity.SILab.Hip.CmsApi.Migrations
+{
+    public partial class authv3adduid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UId",
+                table: "Users",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UId",
+                table: "Users");
+        }
+    }
+}
