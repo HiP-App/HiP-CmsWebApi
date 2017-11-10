96cab580bd00264c90bb3e5da788feecfc6e405c
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-10 12:16:44 +0100

    UserStore-URL is now configurable, updated "appsettings.Development.json.example"

--- src/Api/Managers/UserManager.cs
+++ src/Api/Managers/UserManager.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using PaderbornUniversity.SILab.Hip.CmsApi.Utility;
 using PaderbornUniversity.SILab.Hip.UserStore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,14 +11,14 @@ namespace PaderbornUniversity.SILab.Hip.CmsApi.Managers
 {
     public class UserManager
     {
-        // TODO: make configurable
-        private const string UserStoreUrl = "http://localhost:5000";
-
         private readonly UsersClient _usersClient;
 
-        public UserManager(IHttpContextAccessor context)
+        public UserManager(IHttpContextAccessor context, IOptions<AppConfig> appConfig)
         {
-            _usersClient = new UsersClient(UserStoreUrl)
+            if (string.IsNullOrWhiteSpace(appConfig.Value?.UserStoreUrl))
+                throw new ArgumentException($"The property '{nameof(AppConfig)}.{nameof(AppConfig.UserStoreUrl)}' is not configured");
+
+            _usersClient = new UsersClient(appConfig.Value.UserStoreUrl)
             {
                 Authorization = context.HttpContext.Request.Headers["Authorization"].ToString()
             };
--- src/Api/Utility/AppConfig.cs
+++ src/Api/Utility/AppConfig.cs
@@ -4,6 +4,10 @@
     {
         public string EmailService { get; set; }
 
-        public string AdminEmail { get; set; }
+        /// <summary>
+        /// Base URL of a running HiP-UserStore instance.
+        /// Examples: "http://localhost:5000", "https://docker-hip.cs.upb.de/develop/userstore"
+        /// </summary>
+        public string UserStoreUrl { get; set; }
     }
 }
