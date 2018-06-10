44a5181eb612d9dcef5bc508af250fcf66d8c551
thielep <thielep@mail.uni-paderborn.de>
2018-06-10 16:34:46 +0200

    Removed distinction between develop and master branch from publishing scripts

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -1,10 +1,7 @@
 $csproj = (ls *.Sdk\*.csproj).FullName
-Switch ("$env:Build_SourceBranchName")
-{
-    "master" { dotnet pack "$csproj" -o . }
-    "develop" { dotnet pack "$csproj" -o . --version-suffix "develop" }
-    default { exit }
-}
+
+dotnet pack "$csproj" -o .
+
 $nupkg = (ls src\*.Sdk\*.nupkg).FullName
 dotnet nuget push "$nupkg" -k "$env:MyGetKey" -s "$env:NuGetFeed"
 $LASTEXITCODE = 0
